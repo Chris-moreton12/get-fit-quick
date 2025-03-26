920ba578f8e13caf57759b4705ab5963b0af6c46
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-03-26 18:49:20 +0000

    Set up models and migrations for accounts, products, and subscriptions

---
+++ getfitquick/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for getfitquick project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'getfitquick.settings')
+
+application = get_asgi_application()
