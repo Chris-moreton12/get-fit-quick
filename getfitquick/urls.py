291e315454433027b08ff2097178799886bf8ff0
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-05-21 21:50:17 +0100

    attempt to migrate to postgresql but kept as db.sqlite3, fixed image so products now show images, Set up static and media files correctly for product images,Fixed URL and settings configuration for media handling

--- getfitquick/urls.py
+++ getfitquick/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contact/', views.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
