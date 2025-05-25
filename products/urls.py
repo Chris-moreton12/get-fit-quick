9e06d2d07af7df97f7a044b4088ff8ac0a8ef762
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-05-25 21:30:43 +0100

    product app updated t oallow for tipage name to link to products page

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('', views.product_list, name='product_list'),  # Displays the list of products
     path('products/', views.product_list, name='products'),
