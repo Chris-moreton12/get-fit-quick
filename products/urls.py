e209618aead007ee718db33f0fbdd74ad870c188
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-06-23 22:51:12 +0100

    url.s updated for functionalty, python version changed in runtime

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),  # Displays the list of products
-    path('products/', views.product_list, name='products'),
+    path('', views.product_list, name='product_list'),  # List of products at /products/
 ]
