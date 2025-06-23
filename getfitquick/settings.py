67c839121fc5c258f77a17b02de216434d0a410f
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-06-23 23:32:22 +0100

    depug changed to true to find the bug

--- getfitquick/settings.py
+++ getfitquick/settings.py
@@ -11,6 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-for-dev')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
+DEBUG = True
+
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
