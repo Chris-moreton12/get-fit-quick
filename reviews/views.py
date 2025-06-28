e9e1eb085cadd42d25fb8a7c6e782ffb1ee9af59
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-06-28 21:26:33 +0100

    forms page added, populated, view added for form

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def review_list(request):
+    reviews = Review.objects.order_by('-created_at')
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
+
+@login_required
+def add_review(request):
+    if request.method == 'POST' :
+        form = ReviewForm(request.POST)
+        if form.is_validvalid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('review_list')
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/add_review.html', {'form': form})
