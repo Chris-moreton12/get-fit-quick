3185627fc0a009c831a90e8b6e33ec78f698df9a
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-06-30 22:32:54 +0100

    attmept to fix image bug not rendering

--- products/models.py
+++ products/models.py
@@ -4,7 +4,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='media/product_images', blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
 
     def __str__(self):
         return self.name
