012e6cf863e41152e4c162b4dc7cfa4a810ab4dd
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-07-01 00:16:45 +0100

    Add test script

---
+++ smtp_test.py
@@ -0,0 +1,16 @@
+import smtplib
+import os
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+try:
+    server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+    server.starttls()
+    server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+    print("SMTP login successful")
+    server.quit()
+except Exception as e:
+    print(f"SMTP login failed: {e}")
