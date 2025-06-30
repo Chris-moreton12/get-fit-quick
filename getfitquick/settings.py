dff05842d3c2197488d826ca06791ebedad04e84
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-06-30 22:44:57 +0100

    media url edited

--- getfitquick/settings.py
+++ getfitquick/settings.py
@@ -131,7 +131,7 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-2.amazonaws.com' 
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
